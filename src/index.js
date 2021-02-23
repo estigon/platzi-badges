4b0f03f0e923c2670367d09583e2fc6b993dabb3
Estiben <elysestiben@gmail.com>
2021-02-23 11:19:14 -0400

    diferencias entre uso de jsx y React.createElement

--- src/index.js
+++ src/index.js
@@ -18,8 +18,31 @@ const jsx = <h1>Hello, {nameSite} Badges!</h1>;
 const sum = (a , b) => a + b
 const jsx2 = <h1>pueden usarse entre expresiones { 5 + 3} o llamado a funciones { sum(5, 4) }</h1>
 
+//pueden agregarse varios contenedores
+const jsx3 = <div>
+    <h1>Hola, soy estigon</h1>
+    <p>Trabajo como developer</p>
+</div>
+
+//asi tambien es valido
+const jsx4 = (<div>
+    <h1>Hola, soy estigon</h1>
+    <p>Trabajo como developer</p>
+</div>);
+
+//es mas sencillo con jsx que con createElement
+const element2 = React.createElement(
+    'div', {}, '',
+    React.createElement(
+        'h1', {}, 'Hola, soy estigon',        
+    ),
+    React.createElement(
+        'p', {}, 'Trabajo como developer',        
+    )
+)
+
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(jsx2, container);
+ReactDOM.render(jsx4, container);
 
