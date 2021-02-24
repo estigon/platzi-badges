d5375b385566c8e525a29f81ab0ba311796996b4
Estiben <elysestiben@gmail.com>
2021-02-24 10:26:37 -0400

    agregando estilos y props a un componente

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import confLogo from '../images/badge-header.svg'
 
+//los styles no los almaceno en una variable, al importarlos webpack se encarga de agregarlos al codigo
+import './styles/Badge.css'
+
 class Badge extends React.Component {
     render(){
         return(
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="logo de la conferencia" />
                 </div>
 
-                <div>
-                    <img src="https://es.gravatar.com/avatar?d=identicon" alt="avatar" />
-                    <h1>Elys <br /> Gonzalez</h1>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://es.gravatar.com/avatar?d=identicon" alt="avatar" />
+                    <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
                 </div>
 
-                <div>
-                    <p>Frontend developer</p>
-                    <p>@Estigon</p>
+                <div className="Badge__section-info">
+                    <h3>{this.props.job}</h3>
+                    <div>@{this.props.twitter}</div>
                 </div>
 
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import Badge from './components/Badge'
 
+//instalar bootstrap primero desde npm
+import 'bootstrap/dist/css/bootstrap.css'
+
+//otros styles globales
+import './global.css'
+
 // const jsx = <div>
 //     <Badge />
 //     <h1>Hola, soy estigon</h1>
@@ -11,5 +17,11 @@ import Badge from './components/Badge'
 
 const container = document.getElementById('app');
 
-ReactDOM.render( <Badge />, container );
+ReactDOM.render( 
+<Badge 
+    firstName="Elys"
+    lastName="Gonzalez"
+    job="Junior developer"
+    twitter="estigon"
+/>, container );
 
