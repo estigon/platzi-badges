3451219a289df2ecb1158cf26f09583b5051203d
Estiben <elysestiben@gmail.com>
2021-02-25 09:20:34 -0400

    agregando el form

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BadgeForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>componente formulario</h1>
+            </div>
+        )
+    }
+}
+
+export default BadgeForm
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar'
 import Badge from '../components/Badge'
 import header from '../images/badge-header.svg'
 import './styles/BadgeNew.css'
+import BadgeForm from '../components/BadgeForm'
+
 class BadgeNew extends React.Component {
     render() {
         return (
@@ -14,13 +16,16 @@ class BadgeNew extends React.Component {
 
                 <div className="container">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-6">
                             <Badge 
                                 firstName="Elys"
                                 lastName="Gonzalez"
                                 job="Junior developer"
                                 twitter="estigon"
                             />
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm />
                         </div>
                     </div>
                 </div>
